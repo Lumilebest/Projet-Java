7250f854e0b63da1e2c4bca6425dd84e5c2ee866
Lumilebest <lumilebest@gmail.com>
2026-04-09 16:44:00 +0200

    amorce

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        //charge le fichier fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+
+        //lit et construit l'affichage a partir du fxml
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+
+        //charge le controller et lui passe le stage en paramètre
+        Controller c = fxmlLoader.getController();
+        c.setStage(stage);
+
+        //donne un titre puis met la scene a l'affichage puis la montre
+        stage.setTitle("Application projet!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controller {
+    @FXML
+    private Image img;
+    @FXML
+    private ImageView imgv;
+    private Stage stage;
+
+    protected void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+    @FXML
+    protected void choosePhotoButton() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisis une image");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Images", "*.jpg", "*.jpeg", "*.png", "*.gif", "*.bmp")
+        );
+        File dossier = new File("src/main/resources/images");
+        if ( dossier.exists()){
+            fileChooser.setInitialDirectory(dossier);
+        }
+
+        File choix = fileChooser.showOpenDialog(this.stage);
+
+
+        this.img = new Image(choix.toURI().toString());
+        this.imgv.setImage(img);
+    }
+
+    protected  void rotatePhoto(){
+        PixelReader pr = this.img.getPixelReader();
+        int l = (int)this.img.getWidth();
+        int h = (int)this.img.getHeight();
+
+    }
+
+}
